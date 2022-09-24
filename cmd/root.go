982857a7faab84c23e87fc3304fb306166651655
alexandram <alexandram@vmware.com>
2022-09-24 00:35:28 -0400

    updated short descr in root file

--- cmd/root.go
+++ cmd/root.go
@@ -28,13 +28,9 @@ var cfgFile string
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "todo",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Quick way to keep track of todo items",
+	Long: `todo is a CLI library that can be used to keep track of your todo list.
+It's a convenient way to keep track of an items status and due date.`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	// Run: func(cmd *cobra.Command, args []string) { },
